64f00863e175446d87fe62ea5766ffb9e1e5a7c6
Matthias Enzi <matthias@upleveled.io>
2021-01-24 21:12:08 +0100

    Add loop to download images into folder.

--- index.js
+++ index.js
@@ -1,35 +1,9 @@
-// 1. Get HTML from URL - Node-Fetch to console log HTML from the URL
-
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const https = require('https');
 
-fetch('https://memegen-link-examples-upleveled.netlify.app/')
-  .then((res) => res.text())
-  .then((html) => {
-    // 2. Identify all the pictures on the website.
-
-    const $ = cheerio.load(html); // gets the HTML code of the entire website.
-
-    const ImagesArray = []; //the loop sorts out the first 10 images and puts them into an array.
-
-    $("[href$='.jpg']").each(function () {
-      const link = $(this).html().split('"');
-      ImagesArray.push(link[1]);
-    });
-
-    for (let i = 0; i < 10; i++) {
-      const Images = ImagesArray[i];
-
-      const FinalImagesArray = []; // first 10 images are put to an array.
-      FinalImagesArray.push(Images);
-
-      console.log(FinalImagesArray);
-    }
-    $.html();
-  });
-
-// 3. Create a memes folder if it doesn't exist yet
+// Create a memes folder if it doesn't exist yet
 
 const dir = './memes';
 try {
@@ -40,8 +14,42 @@ try {
   console.error(err);
 }
 
-// 6. Loop over the array for 10 times.
+// Get HTML from URL - Node-Fetch to console log HTML from the URL
 
-// 7. Download the image information.
+fetch('https://memegen-link-examples-upleveled.netlify.app/')
+  .then((res) => res.text())
+  .then((html) => {
+    // Identify all the pictures on the website and gets the HTML code of the entire website.
 
-// 8. Creat a new file with this image information
+    const $ = cheerio.load(html);
+
+    const imagesArray = [];
+    const finalImagesArray = [];
+
+    // splits and gets the separated URLs of the images.
+
+    $("[href$='.jpg']").each(function () {
+      const link = $(this).html().split('"');
+      imagesArray.push(link[1]);
+    });
+
+    // Loop to sort out the first 10 images and put them into an array.
+
+    for (let i = 0; i < 10; i++) {
+      const images = imagesArray[i];
+      finalImagesArray.push(images);
+    }
+
+    // Loop to download the first 10 images and save them to the new folder 'memes'.
+
+    for (i = 0; i < 10; i++) {
+      const file = fs.createWriteStream(`./memes/memes${i}.jpg`);
+      const requestNew = https.get(finalImagesArray[i], function (response) {
+        response.pipe(file);
+      });
+    }
+
+    $.html();
+
+    console.log('Images downloaded');
+  });
